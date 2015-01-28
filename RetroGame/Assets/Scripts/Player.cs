fe27935874be111b5cf2a1f15a19fe7cd3336348
Atte Vuorinen <AtteVuorinen@gmail.com>
2015-01-28 21:52:17 +0200

    Updated Player.cs, added game over logic.

--- RetroGame/Assets/Scripts/Player.cs
+++ RetroGame/Assets/Scripts/Player.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
-
 	[SerializeField]
 	private float m_movementSpeed;
 
@@ -11,7 +10,13 @@ public class Player : MonoBehaviour
 	private float m_jumpPower;
 	
 	private bool m_isGrounded;
-
+	
+	/// <summary>
+	/// Camera for checking is player on screen.
+	/// </summary>
+	private Transform m_mainCameraTransform;
+	private Bounds m_mainCameraBounds;
+	
 	/// <summary>
 	/// Cached Transform.
 	/// </summary>
@@ -22,16 +27,35 @@ public class Player : MonoBehaviour
 	/// </summary>
 	private new Rigidbody2D rigidbody2D;
 	
+	private Vector3 BoundsCenter
+	{
+		get
+		{
+			Vector3 newPosition = m_mainCameraTransform.position;
+			newPosition.z = transform.position.z;
+			
+			return newPosition;
+		}
+	}
+	
 	private void Awake()
 	{
 		transform = GetComponent<Transform>();
 		rigidbody2D = GetComponent<Rigidbody2D>();
+		m_mainCameraTransform = Camera.main.transform;
+		
+		if( m_mainCameraTransform )
+		{
+			// Sets bounds size to match camera size.
+			Vector3 boundsSize = m_mainCameraTransform.camera.ViewportToWorldPoint( Vector3.one * 1.5f );
+			boundsSize.z = 2;
+			m_mainCameraBounds = new Bounds( BoundsCenter, boundsSize );
+		}
 	}
 	
 	private void Update()
 	{
 		float horizontalAxis = Input.GetAxis("Horizontal");
-		
 		transform.Translate( Vector3.right * horizontalAxis * m_movementSpeed * Time.deltaTime );
 	}
 	
@@ -42,8 +66,20 @@ public class Player : MonoBehaviour
 			rigidbody2D.velocity = Vector2.zero;
 			rigidbody2D.AddForce( Vector2.up * m_jumpPower, ForceMode2D.Impulse );
 		}
-	}
+		
+		if( m_mainCameraTransform )
+		{
+			m_mainCameraBounds.center = BoundsCenter;
+			
+			if( !m_mainCameraBounds.Contains( transform.position ) )
+			{
+				// TODO: 'Real' GameOver!
+				Debug.Log("Game Over!");
+			}
+		}
 	
+	}
+
 	private void OnCollisionStay2D(Collision2D collision)
 	{
 		if( collision.contacts[0].point.y < transform.position.y ) 
